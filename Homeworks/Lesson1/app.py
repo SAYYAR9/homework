8dc15396ca20217afa2480967e0c6dd41c1b02b3
SAYYAR9 <seyyar.huseynov.2003@gmail.com>
2024-05-21 17:50:35 +0400

    lesson1 update

--- Homeworks/Lesson1/app.py
+++ Homeworks/Lesson1/app.py
@@ -1,2 +1,10 @@
-print('salam')
-print('sagol')
+val = int(input('ne qeder kredit isteyirsiniz?: '))
+
+if val<2000:
+    print('2000nin altinda kredit verilmir')
+elif 2000 < val and val< 10000:
+    print('5faiz kredit vere bilirik')
+elif val<15000:
+    print('3faiz vere bilirik')
+else:
+    print('15000den yuxari kredit vermirik')
