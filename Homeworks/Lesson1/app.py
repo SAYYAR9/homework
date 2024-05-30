4b9f015a35802e3460e366209c3a2058128090df
SAYYAR9 <seyyar.huseynov.2003@gmail.com>
2024-05-30 17:00:24 +0400

    lesson1 update and string metods

--- Homeworks/Lesson1/app.py
+++ Homeworks/Lesson1/app.py
@@ -20,16 +20,30 @@
 # data = '{} {}-da {} kurs telebesidir'
 # print(data.format(telebe,uni,kurs))
 
-password = input('kodu daxil et: ')
-if not len(password)>8 and len(password)<40:
-    print('Ən az 8 və ən çox 40 characterdən ibarət olmalıdır')
-elif not password.isascii():
-    print('ancaq ingilis shriftlerinden istifade olunmalidir')
-elif not password.isalnum():
-    print('ancaq herf ve reqemden ibaret olmalidir')
-elif password.isupper() or password.islower():
-    print('Mütləq şəkildə ən az bir kicik ve bir boyuk herf olmalidir')
-elif password.isalpha() and password.isalnum():
-    print('en az bir herf ve bir reqem olmalidir')
+# password = input('kodu daxil et: ')
+# if not len(password)>8 and len(password)<40:
+#     print('Ən az 8 və ən çox 40 characterdən ibarət olmalıdır')
+# elif not password.isascii():
+#     print('ancaq ingilis shriftlerinden istifade olunmalidir')
+# elif not password.isalnum():
+#     print('ancaq herf ve reqemden ibaret olmalidir')
+# elif password.isupper() or password.islower():
+#     print('Mütləq şəkildə ən az bir kicik ve bir boyuk herf olmalidir')
+# elif password.isalpha() and password.isalnum():
+#     print('en az bir herf ve bir reqem olmalidir')
+# else:
+#     print('Sifre qebul edildi')
+
+
+number = input('nomreni daxil edin: ')
+
+if len(number) == 13:
+    if number.startswith('+994'):
+        if number[1:14].isnumeric():
+            print('nomreni duzgun daxil etdiniz')
+        else:
+            print('tekce reqem daxil ede bilersiniz')
+    else:
+        print('+994 ile baslamalidir')
 else:
-    print('Sifre qebul edildi')
+    print('nomrenin uzunlugu duzgun deyil')
